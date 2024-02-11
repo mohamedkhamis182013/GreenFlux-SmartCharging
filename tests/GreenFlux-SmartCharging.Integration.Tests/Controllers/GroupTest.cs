61c1f662af7c546bbe77a6012a8d67682145d0fc
mohamedkhamis182013 <mohamed.khamis182013@gmail.com>
2024-02-11 11:05:43 +0100

    add more tests

--- tests/GreenFlux-SmartCharging.Integration.Tests/Controllers/GroupTest.cs
+++ tests/GreenFlux-SmartCharging.Integration.Tests/Controllers/GroupTest.cs
@@ -1,7 +1,10 @@
 ﻿using System.Net;
+using System.Text;
 using FluentAssertions;
+using GreenFlux_SmartCharging.Application.Common.Extensions;
 using GreenFlux_SmartCharging.Infrastructure.Persistence;
 using GreenFlux_SmartCharging.Tests.Common.Builders;
+using Newtonsoft.Json;
 
 namespace GreenFlux_SmartCharging.Integration.Tests.Controllers
 {
@@ -10,8 +13,16 @@ namespace GreenFlux_SmartCharging.Integration.Tests.Controllers
         [Fact]
         public async Task GettingAllGroups_WhenDataExist_ShouldReturn200OKResult()
         {
-            var group1 = new GroupBuilder().WithId(Guid.NewGuid()).WithName("g1").WithCapacity(500).Build();
-            var group2 = new GroupBuilder().WithId(Guid.NewGuid()).WithName("g2").WithCapacity(800).Build();
+            var group1 = new GroupBuilder()
+                .WithId(Guid.NewGuid())
+                .WithName("g1")
+                .WithCapacity(500)
+                .Build();
+            var group2 = new GroupBuilder()
+                .WithId(Guid.NewGuid())
+                .WithName("g2")
+                .WithCapacity(800)
+                .Build();
 
             await using (var dbContext = new AppDbContext(TestDb.ContextOptions))
             {
@@ -30,6 +41,35 @@ namespace GreenFlux_SmartCharging.Integration.Tests.Controllers
 
             response.StatusCode.Should().Be(HttpStatusCode.NotFound);
         }
+        [Fact]
+        public async Task CreateGroup_WhenModelIsValid_ShouldReturnOkStatus()
+        {
+            var group1 = new GroupBuilder()
+                .WithId(Guid.NewGuid())
+                .WithName("g1")
+                .WithCapacity(500)
+                .Build();
+            var groupDto = group1.ToGroupDto();
+            var response = await Client.PostAsync("/api/Group/Create",
+                new StringContent(JsonConvert.SerializeObject(groupDto),
+                    Encoding.UTF8, "application/json")
+            );
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
 
+        [Fact]
+        public async Task CreateGroup_WhenModelIsNotValid_ShouldReturnStatus()
+        {
+            var group1 = new GroupBuilder()
+                .WithId(Guid.NewGuid())
+                .WithCapacity(500)
+                .Build();
+            var groupDto = group1.ToGroupDto();
+            var response = await Client.PostAsync("/api/Group/Create",
+                new StringContent(JsonConvert.SerializeObject(groupDto),
+                    Encoding.UTF8, "application/json")
+            );
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
     }
 }
---
+++ tests/GreenFlux-SmartCharging.api.Tests/Controllers/GroupControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using GreenFlux_SmartCharging.api.Controllers;
+using GreenFlux_SmartCharging.Application.Common.Extensions;
+using GreenFlux_SmartCharging.Application.Common.Interfaces;
+using GreenFlux_SmartCharging.Tests.Common.Builders;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace GreenFlux_SmartCharging.api.Tests.Controllers
+{
+    public class GroupControllerTests
+    {
+        [Fact]
+        public async Task CreateAsync_WhenModelIsValid_ShouldReturnOKResult()
+        {
+            var group1 = new GroupBuilder()
+                .WithId(Guid.NewGuid())
+                .WithName("g1")
+                .WithCapacity(500)
+                .Build();
+            var groupDto = group1.ToGroupDto();
+            var groupServiceMock = new Mock<IGroupService>();
+            var sut = new GroupController(groupServiceMock.Object);
+            var actualResult = await sut.CreateAsync(groupDto);
+
+            actualResult.Should().BeOfType<OkResult>();
+        }
+    }
+}
